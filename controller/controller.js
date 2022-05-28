b0fe3ffcc97b9a687c79cc8be2280c8fc3c49a8f
vutranhoang <hoang.vu141102@hcmut.edu.vn>
2022-05-28 09:44:05 +0700

    asdasd

--- controller/controller.js
+++ controller/controller.js
@@ -2,17 +2,28 @@ const knex = require ('../model/connectDB');
 const canvas = require('canvas');
 const path = require('path');
 const socketio = require('../socket/socketio');
-let imgData = require('../testing.json');
+let imgData = require('../data/faceData.js');
 const faceapi = require('@vladmandic/face-api');
+let count=0;
 const confirmCheckin = async(req,res,next)=>{
     try{
         const {ID}=req.body;
-        // await knex('attendances').where({
-        //     ID:ID,
-        // }).update({
-        //     check_in:'1',
-        // })
-        console.log(ID);
+
+        //update db
+        await knex('attendances').where({
+            ID:ID,
+        }).update({
+            check_in:'1',
+        })
+        count++;
+        //get name
+        const {name} = await knex('attendances').where({
+            ID:ID,
+        }).first();
+        ////push to socket
+        socketio.getIO().emit('student_join',{name:name,status:"in"});
+
+        //server response
         res.json({
             msg:'success'
         })
@@ -24,13 +35,20 @@ const confirmCheckin = async(req,res,next)=>{
 const confirmCheckout= async(req,res,next)=>{
     try{
         const {ID}=req.body;
-        // await knex('attendances').where({
-        //     ID:ID,
-        //     check_in:'1'
-        // }).update({
-        //     check_out:'1',
-        // })
-        console.log(ID);
+
+        ///update db
+        await knex('attendances').where({
+            ID:ID,
+            check_in:'1'
+        }).update({
+            check_out:'1',
+        })
+        count--;
+        const {name} = await knex('attendances').where({
+            ID:ID,
+        }).first();
+        socketio.getIO().emit('student_join',{name:name,status:"out"});
+        ///server response
         res.json({
             msg:'success'
         })
@@ -57,14 +75,13 @@ const userCheckin = async(req,res,next)=>{
 
         // const result1 = await detectSingleFace(c1,new SsdMobilenetv1Options()).withFaceLandmarks().withFaceDescriptor();
         // const result2 = await detectSingleFace(c2,new SsdMobilenetv1Options()).withFaceLandmarks().withFaceDescriptor();
-        for (let i=0;i<imgData.length;i++){
-            imgData[i]=new faceapi.LabeledFaceDescriptors(imgData[i].label,[new Float32Array(imgData[i].descriptors[0])]);
-        }
+
+        
         // console.log(imgData[0] instanceof faceapi.LabeledFaceDescriptors);
         
         // imgData.map(element=>new faceapi.LabeledFaceDescriptors(element.label,[new Float32Array(element.descriptors[0])]));
 
-        const faceMatcher = new faceapi.FaceMatcher(imgData,0.5);
+        const faceMatcher = new faceapi.FaceMatcher(imgData,0.45);
         const bestMatch = faceMatcher.findBestMatch(req.body.data);
 
         // const bestMatch = faceMatcher.findBestMatch(result2.descriptor);
@@ -72,22 +89,29 @@ const userCheckin = async(req,res,next)=>{
         if (bestMatch.label==='unknown'){
             res.json({
                 identify:false,
-                msg:`cant identify user`
+                msg:`cant identify student`
             })
         }
         else{
-            //update db
+            //query db
+            const data = await knex('attendances').select('ID','name').where({
+                ID:bestMatch.label
+            }).first();
 
-            ////push to socket
-            socketio.getIO().emit('student_join',bestMatch.label);
-
-            //server responsex
-            res.json({
-                identify:true,
-                className:"mt20kh10",
-                ID:2013245,
-                name:bestMatch.label
-            });
+            if (data){
+                res.json({
+                    identify:true,
+                    ID:data.ID,
+                    name:data.name
+                });
+            }
+            else{
+                res.json({
+                    identify:false,
+                    msg:"can't find you student ID",
+                })
+            }
+            
         } 
     }catch(error){
         console.log(error);
@@ -103,13 +127,17 @@ const emit_testing = async(req,res,next)=>{
     })
 }
 
-const render_in = async(req,res,next)=>{
-    res.render('in');
+const render_checkin = async(req,res,next)=>{
+    res.render('checkin');
+}
+const render_checkout = async(req,res,next)=>{
+    res.render('checkout')
 }
 const render_out = async(req,res,next)=>{
-    res.render('out');
+
+    res.render('out',{count});
 }
 const render_test = async(req,res,next)=>{
     res.render('test');
 }
-module.exports={userCheckin,emit_testing,render_in,render_out,render_test,confirmCheckin,confirmCheckout}
+module.exports={userCheckin,emit_testing,render_checkin,render_checkout,render_out,render_test,confirmCheckin,confirmCheckout}
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,12 @@
 const express=require('express');
 const routes=express.Router();
-const { userCheckin, emit_testing, render_in, render_out, render_test, confirmCheckout, confirmCheckin } = require('../controller/controller.js');
+const { userCheckin, emit_testing, render_checkin, render_checkout,render_out, render_test, confirmCheckout, confirmCheckin } = require('../controller/controller.js');
 routes.route('/checkin').post(userCheckin)
 routes.route('/confirm/checkin').post(confirmCheckin);
 routes.route('/confirm/checkout').post(confirmCheckout);
 routes.route('/emit_testing').post(emit_testing);
-routes.route('/in').get(render_in);
+routes.route('/checkin').get(render_checkin);
+routes.route('/checkout').get(render_checkout);
 routes.route('/out').get(render_out);
 routes.route('/test').get(render_test);
 
